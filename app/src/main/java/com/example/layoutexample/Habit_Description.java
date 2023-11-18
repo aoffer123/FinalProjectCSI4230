844dfb6c3d3922a71eb3533327d14b6dd428be3d
Denise Rauschendorfer <denise.n.rauschendorfer@gmail.com>
2023-11-18 15:10:02 -0500

    adding habitDesc page

---
+++ app/src/main/java/com/example/layoutexample/Habit_Description.java
@@ -0,0 +1,14 @@
+package com.example.layoutexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Habit_Description extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_description);
+    }
+}
